9c7a5083c71efdb3a9ef1d340b198de367b3ab13
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2019-08-13 06:11:05 -0600

    resize input box, add issues/tasks

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const addToDo = () => {
 /* ISSUES/TASKS
 --keep items painted on screen for next item submission DONE
 --checkbox to marked when item is completed (line through completed item)
+--when user clicks enter, item gets added to list
 --save items in local storage?
 --add a service worker
 --add list themes
