5acc979752aedc5492f125438e02aff8eb4ab613
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2019-08-13 05:36:11 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let toDoList = "";
+
+const addToDo = () => {
+  let userInput = document.getElementById('userInput').value;
+  toDoList += `<h3>${userInput}</h3>`;
+  document.getElementById('itemList').innerHTML = toDoList;
+  //clear input field after submission
+  document.getElementById('userInput').value = "";
+}
+
+
+
+
+
+
+
+
+
+
+/* ISSUES/TASKS
+--keep items painted on screen for next item submission DONE
+--checkbox to marked when item is completed (line through completed item)
+--save items in local storage?
+--add a service worker
+--add list themes
+*/
